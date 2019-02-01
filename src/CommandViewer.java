cd5c94f464f7b130a09214334b30b28336cfca84
Kevin Lalor <kevin.lalor@ucdconnect.ie>
2019-02-01 10:47:32 +0000

    FirstCommit

---
+++ src/CommandViewer.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+/**
+   This program displays the growth of an investment. 
+*/
+public class CommandViewer
+{  
+   public static void main(String[] args)
+   {  
+      JFrame CommandPanel = new CommandPanel();
+      CommandPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      CommandPanel.setVisible(true);
+   }
+}
+
