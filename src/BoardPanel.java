6440a863b8e5ddce7bfef7de031d3d63bb7cb1f3
samoneill1122 <33814455+samoneill1122@users.noreply.github.com>
2019-02-08 20:39:00 +0000

    Update BoardPanel.java

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -14,6 +14,8 @@ import javax.swing.JPanel;
 public class BoardPanel extends JPanel {
 	
 	private BufferedImage boardImage;
+	private Checker[] whiteCheckers = new Checker[15];
+	private Checker[] blackCheckers = new Checker[15];
 	
 	public BoardPanel()
 	{
@@ -37,60 +39,59 @@ public class BoardPanel extends JPanel {
 
 		 g2.drawImage(boardImage, 0, 0, 600, 500, this);
 		 
-		 int y = 435;
-		 
-		 // Adding white checkers to the board.
-		 while(y > 334)
+		 int i=0;
+		 // Creating and drawing the white checkers
+		 int y=435;
+		 for(i=0; i<5; i++)
 		 {
-			 Checker newCheckerWhite = new Checker(Color.WHITE);
-			 newCheckerWhite.drawChecker(325, y, g, Color.WHITE, Color.BLACK);
+			 whiteCheckers[i] = new Checker(Color.WHITE, Color.BLACK, g2, 325, y);
 			 y -= 20;
 		 }
-		 y = 435;
-		 while(y > 394)
+		 y=435;
+		 for(i=5; i<8; i++)
 		 {
-			 addCircle(190, y, g, Color.WHITE, Color.BLACK);
+			 whiteCheckers[i] = new Checker(Color.WHITE, Color.BLACK, g2, 190, y);
 			 y -= 20;
 		 }
-		 y = 25;
-		 while(y < 46)
+		 y=25;
+		 for(i=8; i<13; i++)
 		 {
-			 addCircle(532, y, g, Color.WHITE, Color.BLACK);
+			 whiteCheckers[i] = new Checker(Color.WHITE, Color.BLACK, g2, 27, y);
 			 y += 20;
 		 }
-		 y = 25;
-		 while(y < 126)
+		 y=25;
+		 for(i=13; i<15; i++)
 		 {
-			 addCircle(27, y, g, Color.WHITE, Color.BLACK);
-			 y += 20;
-		 }
-		 
-		 // Adding black checkers to the board.
-		 y = 435;
-		 while(y > 414)
-		 {
-			 addCircle(532, y, g, Color.BLACK, Color.WHITE);
-			 y -= 20;
-		 }
-		 y = 25;
-		 while(y < 126)
-		 {
-			 addCircle(325, y, g, Color.BLACK, Color.WHITE);
-			 y += 20;
-		 }
-		 y = 435;
-		 while(y > 334)
-		 {
-			 addCircle(27, y, g, Color.BLACK, Color.WHITE);
-			 y -= 20;
-		 }
-		 y = 25;
-		 while(y < 66)
-		 {
-			 drawCheckers(193, y, g, Color.BLACK, Color.WHITE);
+			 whiteCheckers[i] = new Checker(Color.WHITE, Color.BLACK, g2, 532, y);
 			 y += 20;
 		 }
 		 
+		 // Creating and drawing the black checkers
+		 y=25;
+		 for(i=0; i<5; i++)
+		 {
+			 blackCheckers[i] = new Checker(Color.BLACK, Color.WHITE, g2, 325, y);
+			 y += 20;
+		 }
+		 y=25;
+		 for(i=5; i<8; i++)
+		 {
+			 blackCheckers[i] = new Checker(Color.BLACK, Color.WHITE, g2, 193, y);
+			 y += 20;
+		 }
+		 y=435;
+		 for(i=8; i<13; i++)
+		 {
+			 blackCheckers[i] = new Checker(Color.BLACK, Color.WHITE, g2, 27, y);
+			 y -= 20;
+		 }
+		 y=435;
+		 for(i=13; i<15; i++)
+		 {
+			 blackCheckers[i] = new Checker(Color.BLACK, Color.WHITE, g2, 532, y);
+			 y -= 20;
+		 }
+
 		 g2.drawRect(610, 20, 55, 185);
 		 g2.drawRect(610, 300, 55, 185);
 	}
