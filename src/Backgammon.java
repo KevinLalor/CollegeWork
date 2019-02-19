ffca471e860874c2cca30181f682d734248bdef2
PhantomSean <44501054+PhantomSean@users.noreply.github.com>
2019-02-19 13:30:10 +0000

    Sprint 1 transfer
    Initial code that was taken from Sprint 1 solutions

---
+++ src/Backgammon.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeUnit;
+
+public class Backgammon {
+
+    public static final int NUM_PLAYERS = 2;
+
+    private final Board board = new Board();
+    private final UI ui = new UI(board);
+
+    private void testUI() throws InterruptedException {
+        // Moves a checker on to the bar and then moves it one pip at a time around the board.
+        // Does this for both players.
+        ui.display();
+        for (int player=0; player<NUM_PLAYERS; player++) {
+            board.move(player,Board.NUM_PIPS,Board.BAR);
+            ui.display();
+            TimeUnit.SECONDS.sleep(1);
+            for (int pip=Board.BAR; pip>0; pip--) {
+                board.move(player, pip, pip-1);
+                ui.display();
+                TimeUnit.SECONDS.sleep(1);
+            }
+        }
+        String message;
+        do {
+            message = ui.getCommand();
+            ui.displayString(message);
+        } while (!message.equals("quit"));
+    }
+
+    public static void main (String[] args) throws InterruptedException {
+        Backgammon game = new Backgammon();
+        game.testUI();
+        System.exit(0);
+    }
+}
