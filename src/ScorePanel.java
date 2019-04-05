23d2b0005b0e4e4cd637b79605417405d44ad384
samoneill1122 <33814455+samoneill1122@users.noreply.github.com>
2019-04-06 00:01:35 +0100

    Update ScorePanel.java

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -17,7 +17,7 @@ public class ScorePanel extends JPanel{
         score.setFont(new Font("monospaced", Font.PLAIN, FONT_SIZE));
         score.setLineWrap(true);
         score.setWrapStyleWord(true);
-        score.setText("Player 1 Score: 0 | Player 2 Score: 0");
+        score.setText("Player 1 Score: 0 | Player 2 Score: 0 | Game Point: 1 | Match Point: 0");
 		setLayout(new BorderLayout());
 		add(score, BorderLayout.CENTER);
 	}
