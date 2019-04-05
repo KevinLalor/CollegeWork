35e8a725e14ea85f4c155e90c82e46b8097152e5
samoneill1122 <33814455+samoneill1122@users.noreply.github.com>
2019-04-05 18:37:02 +0100

    Update ScorePanel.java

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -1,4 +1,25 @@
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
 
-public class ScorePanel {
+public class ScorePanel extends JPanel{
+	
+	/*private static final int TEXT_AREA_HEIGHT = 40;
+    private static final int CHARACTER_WIDTH = 47;*/
+    private static final int FONT_SIZE = 12;
+	
+	public JTextArea score;
 
+	public ScorePanel()
+	{
+		score = new JTextArea();
+		score.setEditable(false);
+        score.setFont(new Font("monospaced", Font.PLAIN, FONT_SIZE));
+        score.setLineWrap(true);
+        score.setWrapStyleWord(true);
+        score.setText("Player 1 Score: 0 | Player 2 Score: 0");
+		setLayout(new BorderLayout());
+		add(score, BorderLayout.CENTER);
+	}
+	
 }
