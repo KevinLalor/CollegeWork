6d6aeebcef0cf12e8ad6a110b286018d0d6ad57a
PhantomSean <44501054+PhantomSean@users.noreply.github.com>
2019-04-22 03:35:34 +0100

    Sean - addition of incomplete functions
    countDiff() - difference in moves needed to bear off all pips
    blockBlotDiff() - difference in blocks and blots

--- src/Bot0.java
+++ src/Bot0.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Bot0 implements BotAPI {
 
@@ -26,13 +27,97 @@ public class Bot0 implements BotAPI {
         return "Bot0"; // must match the class name
     }
 
+    //Gives score to which move is best
     public String getCommand(Plays possiblePlays) {
         // Add your code here
-        return "1";
+    	int score[] = new int [possiblePlays.number()];
+    	Arrays.fill(score, 0);
+    	score[countDiff(possiblePlays)]++;
+    	score[blockBlotDiff(possiblePlays)]++;
+    	
+    	int command = 1;
+    	for(int i = 0; i < possiblePlays.number(); i++) {
+    		if(i != 0) {
+    			if(score[i] > score[i -1]) {
+    				command = i + 1;
+    			}
+    		} else {
+    			command = 1;
+    		}
+    	}
+        return Integer.toString(command);
     }
 
     public String getDoubleDecision() {
         // Add your code here
         return "n";
+    }
+    
+    // Counts how many moves needed to bear off all pieces
+    public int countDiff(Plays possiblePlays) {
+    	int [][] currentPipLocations = board.get();
+    	int p0 = 0, p1 = 0;
+    	int command = 0;
+
+    	//Counts the moves needed for players from current position of pips
+    	for(int i = 0; i < 25; i++) {	
+    		if(currentPipLocations[me.getId()][i] >= 1) {
+    			p0 += i * currentPipLocations[me.getId()][i];
+    			
+    		}
+    		else if(currentPipLocations[opponent.getId()][i] >= 1) {
+    			p1 += i * currentPipLocations[opponent.getId()][i];
+    		} 
+    	}
+
+    	int count1 = 0;
+    	String commandString;
+		
+    	for(int j = 0; j < possiblePlays.number(); j++) {
+    		commandString = Integer.toString(j+1);
+			Command checkCommand = new Command(commandString, possiblePlays);
+			
+			count1 = newBoard(checkCommand.getPlay());
+			// Need to add in a way to check if the play includes a hit or not
+			if(count1 < p0) {
+				command = j;
+			} 
+		}
+    	return command;
+    }
+    
+    //Check the difference in blocks of player and blots of opposing player
+    //Incomplete: need to check blocks on new board of each play
+    public int blockBlotDiff(Plays possiblePlays) {
+    	int blocks = 0, blots = 0, diff = 0;
+    	int [][] currentPipLocations = board.get();
+    	
+    	for(int i = 0; i < 25; i++) {
+    		if(currentPipLocations[me.getId()][i] > 1) {
+    			blocks++;
+    		}
+    		if(currentPipLocations[opponent.getId()][i] == 1) {
+    			blots++;
+    		}
+    	}
+    	
+    	diff = blocks-blots;
+    	return diff;
+    }
+    
+    //Creates a new board to view look at a play
+    public int newBoard(Play play) {
+    	Players players = new Players();
+    	Player currentPlayer = new Player(me.getId(), me.getColorName(), me.getColor());
+    	Board newBoard = new Board(players);
+    	newBoard.move(currentPlayer, play);
+    	
+    	int newCount = 0;
+    	int[][] newPipLocations = newBoard.get();
+    	
+    	for(int i = 0; i < 25; i++) {
+    		newCount += i * newPipLocations[me.getId()][i];
+    	}
+    	return newCount;
     }
 }
